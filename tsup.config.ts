6d9c30276b899e95c6a0cea88556aef90a2d5587
adrienne-rio <adrienne@deriv.com>
2024-03-08 15:41:18 +0800

    feat: added bootstrap for folders

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,15 @@
 import { defineConfig } from 'tsup';
+import { execSync } from 'child_process';
+import { join } from 'path';
+import fs from 'fs-extra';
 
+const copyTemplatesToBin = async () => {
+    const templatePath = join(__dirname, 'src/templates');
+    fs.copySync(templatePath, './bin/src/templates', {
+        overwrite: true,
+    });
+    execSync('node bin/index.js');
+};
 const isDev = process.env.npm_lifecycle_event === 'dev';
 
 export default defineConfig({
@@ -9,6 +19,6 @@ export default defineConfig({
     minify: !isDev,
     target: 'esnext',
     outDir: 'bin',
-    onSuccess: isDev ? 'node bin/index.js' : undefined,
+    onSuccess: copyTemplatesToBin,
     sourcemap: true,
 });
